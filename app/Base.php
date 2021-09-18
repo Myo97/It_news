d5817df35dc552e7f351c5a2b9a424a12c9f2477
Myo97 <myothetnaing.mtn97@gmail.com>
2021-09-18 15:45:08 +0630

    v.1.0

---
+++ app/Base.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class Base
+{
+    public static $name = "IT News";
+    public static $logo = 'img/logos/logo.PNG';
+    public static $description = "This is my first SSR project";
+    public static function myName(){
+        return "Myo Thet Naing";
+    }
+}
